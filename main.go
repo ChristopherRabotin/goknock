138909385f64df63b90421c41a0d9c3af6fb45c7
Christopher Rabotin <chris@plusponics.com>
2015-03-01 01:27:56 +0000

    Removed unused handler. Added an immortality channel.

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ func main(){
     // Define the sequence and its action here.
     knockseq := NewSequence("Echo action", "127.0.0.1", []int{1024, 1025, 1026}, Echo{}, true)
     // #####
+    immortality := make(chan bool, 1)
     // Main infinite loop to make sure we're always running.
     // Also allows for multiple clients.
-    knockseq.ListenCarefully()
+    go knockseq.ListenCarefully()
+    <- immortality 
 }
