bfa2e502f5121dd976f8177d5c71f353a0ed05e9
Christopher Rabotin <chris@plusponics.com>
2015-03-01 02:10:41 +0000

    Knocks can now have their respective timeouts.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 func main(){
     // #####
     // Define the sequence and its action here.
-    knockseq := NewSequence("Echo action", "127.0.0.1", []int{1024, 1025, 1026}, Echo{}, true)
+    knockseq := NewSequence("Echo action", "127.0.0.1", []Knock{{1024, -1}, {1025, 5}, {1026, 5}}, Echo{}, true)
     // #####
     immortality := make(chan bool, 1)
     // Main infinite loop to make sure we're always running.
--- sequence.go
+++ sequence.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 import "net"
+import "time"
+
+type Knock struct {
+    port int
+    timeout int
+}
 
 type Sequence struct {
     name string
     host string
-    ports []int
+    ports []Knock
     action Action
     portit int
     conn net.Conn
@@ -14,19 +20,24 @@ type Sequence struct {
 }
 
 // Easy constructor for Sequence structs.
-func NewSequence(name string, host string, ports []int, action Action, debug bool) *Sequence{
+func NewSequence(name string, host string, ports []Knock, action Action, debug bool) *Sequence{
     return &Sequence{name, host, ports, action, -1, nil, debug}
 }
 
+func NewLocalSequence(ports []Knock, action Action, debug bool) *Sequence{
+    return &Sequence{"Local sequence", "127.0.0.1", ports, action, -1, nil, debug}
+}
+
 // Returns the next available port according to the sequence.
-func (seq *Sequence) nextport() int {
+func (seq *Sequence) next() Knock {
     seq.portit = (seq.portit + 1) % len(seq.ports)
     return seq.ports[seq.portit]
 }
 
 // Returns the formatted host and port.
-func (seq *Sequence) addr() string {
-    return fmt.Sprintf("%s:%d", seq.host, seq.nextport())
+func (seq *Sequence) addr() (string, int){
+    kn := seq.next()
+    return fmt.Sprintf("%s:%d", seq.host, kn.port), kn.timeout
 }
 
 // Returns the next available port according to the sequence.
@@ -36,15 +47,18 @@ func (seq *Sequence) reset() {
 
 // Accepts the initial knock. Does not have any timeout.
 func (seq *Sequence) acceptKnock() {
-    addr := seq.addr()
+    addr, timeout := seq.addr()
     tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
     if err != nil {
         // If we can't resolve the host, we'll panic!
         panic(err)
     }
-
-    netListen, err := net.Listen(tcpAddr.Network(), tcpAddr.String())
+    netListen, err := net.ListenTCP("tcp", tcpAddr)
     defer netListen.Close()
+    if timeout > 0 {
+        netListen.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
+    }
+    
     if err != nil {
         // If this port is taken, let's just go to the next one.
         if seq.debug{
@@ -57,7 +71,7 @@ func (seq *Sequence) acceptKnock() {
     if seq.debug{
         fmt.Println("Listening on", addr, ".")
     }
-    
+
     conn, err := netListen.Accept()
     defer conn.Close()
     
@@ -65,19 +79,25 @@ func (seq *Sequence) acceptKnock() {
         // Client error, reseting the sequence.
         fmt.Println(err)
         seq.reset()
-    }else if seq.portit != len(seq.ports) - 1 && seq.debug {
-        fmt.Println("Successful connection.")
-    }else{
-        if seq.debug{
-            fmt.Println("Launching action %s.", seq.name)
+        if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+            fmt.Println("Timed out")
         }
-        // Launch action!
-        seq.action.launch(conn)
-        // Once this returns let's reset the sequence to the original setting.
-        seq.reset()
+
+    }else{
+        if seq.portit != len(seq.ports) - 1 && seq.debug {
+            fmt.Println("Successful connection.")
+        }else{
+            if seq.debug{
+                fmt.Println("Launching action %s.", seq.name)
+            }
+            // Launch action!
+            seq.action.launch(conn)
+            // Once this returns let's reset the sequence to the original setting.
+            seq.reset()
+        }
+        conn.Close()
     }
     // Moving onto the next port after successful knock and/or action.
-    conn.Close()
     netListen.Close()
     seq.acceptKnock()
 }
